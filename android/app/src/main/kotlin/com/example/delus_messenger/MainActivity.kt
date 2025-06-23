b7435e27d79b93e99d152086d066245b9fc86e07
Dmitry Olegovich Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2025-06-23 19:17:50 +0300

    Initial commit ..

---
+++ android/app/src/main/kotlin/com/example/delus_messenger/MainActivity.kt
@@ -0,0 +1,5 @@
+package messanging.delus.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/common/config.dart
@@ -0,0 +1,40 @@
+/*
+ *  Delus - The Ultimate PHP Social Network Platform
+ *  Apps Configuration
+*/
+
+/* =============================== */
+/* 🔑 Delus Configuration 🔑       */
+/* =============================== */
+
+// Delus API Key & Secret
+const String apiKey = "";
+const String apiSecret = "";
+
+// Delus API URL
+const String apiURL = "https://delus.monster";
+
+// Debugging Mode
+const bool debugEnabled = false;
+
+/* =============================== */
+/* 🔔 OneSignal Configuration 🔔 */
+/* =============================== */
+
+// OneSignal App ID
+const String oneSignalAppId = "";
+
+/* =============================== */
+/* 🛑 DON'T EDIT BELOW THIS LINE 🛑 */
+/* =============================== */
+
+// Delus API Base URL
+const String apiBase = "apis";
+const String apiStack = "php";
+const String apiBaseURL = "$apiURL/$apiBase/$apiStack";
+
+// Audio/Video Call (Not implemented yet)
+bool audioVideoCallEnabled = false;
+
+// Social Login (Not implemented yet)
+bool socialLoginEnabled = false;
